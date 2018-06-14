9ebbe574e248fb13a11827c818a7a700decd8d8c
Alonso Tacanga <alonso.tacanga@gmail.com>
2018-06-13 20:56:41 -0700

    remove ingredients safely

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,6 +14,8 @@ const buildcontrols = (props) => (
         {controls.map(ctrl =>
         <BuildControl 
             added={() => props.ingredientAdded(ctrl.type)} 
+            removed={() => props.ingredientRemoved(ctrl.type)}
+            disabled={props.disabled[ctrl.type]}
             key={ctrl.label} 
             label={ctrl.label} 
         /> )}
